7821aea9f86f4d25fd28169fbb392c432b1697af
Matthew Koczwara <mattkoczwara@gmail.com>
2014-03-17 20:19:19 -0400

    Cabs/Cota/Stops

--- gen/com/example/columbus/R.java
+++ gen/com/example/columbus/R.java
@@ -209,22 +209,29 @@ containing a value of this type.
         public static final int ic_plusone_tall_off_client=0x7f02001d;
     }
     public static final class id {
-        public static final int about=0x7f050011;
-        public static final int button_home=0x7f05000a;
-        public static final int button_send=0x7f050008;
-        public static final int button_work=0x7f05000c;
-        public static final int cabs_information=0x7f05000e;
-        public static final int cota_information=0x7f05000d;
-        public static final int edit_message=0x7f050006;
+        public static final int about=0x7f050018;
+        public static final int action_settings=0x7f050013;
+        public static final int button_home=0x7f05000c;
+        public static final int button_send=0x7f05000a;
+        public static final int button_work=0x7f05000e;
+        public static final int cabs_information=0x7f050015;
+        public static final int cota_information=0x7f050014;
+        public static final int editText1=0x7f050011;
+        public static final int edit_message=0x7f050008;
         public static final int hybrid=0x7f050004;
-        public static final int linearLayout1=0x7f050005;
-        public static final int linearLayout2=0x7f050007;
-        public static final int linearLayout3=0x7f05000b;
-        public static final int linearLayout4=0x7f050009;
-        public static final int nearby_stops=0x7f05000f;
+        public static final int lblListHeader=0x7f05000f;
+        public static final int lblListItem=0x7f050010;
+        public static final int linearLayout1=0x7f050007;
+        public static final int linearLayout2=0x7f050009;
+        public static final int linearLayout3=0x7f05000d;
+        public static final int linearLayout4=0x7f05000b;
+        public static final int lvExp=0x7f050006;
+        public static final int lvExp2=0x7f050005;
+        public static final int lvExp3=0x7f050012;
+        public static final int nearby_stops=0x7f050016;
         public static final int none=0x7f050000;
         public static final int normal=0x7f050001;
-        public static final int preferences=0x7f050010;
+        public static final int preferences=0x7f050017;
         public static final int satellite=0x7f050002;
         public static final int terrain=0x7f050003;
     }
@@ -232,14 +239,21 @@ containing a value of this type.
         public static final int google_play_services_version=0x7f070000;
     }
     public static final class layout {
-        public static final int activity_main=0x7f030000;
-        public static final int splash=0x7f030001;
+        public static final int activity_cabs_information=0x7f030000;
+        public static final int activity_cota_information=0x7f030001;
+        public static final int activity_main=0x7f030002;
+        public static final int list_group=0x7f030003;
+        public static final int list_item=0x7f030004;
+        public static final int nearest_stops=0x7f030005;
+        public static final int splash=0x7f030006;
     }
     public static final class menu {
-        public static final int main=0x7f0a0000;
+        public static final int cota_information=0x7f0a0000;
+        public static final int main=0x7f0a0001;
     }
     public static final class string {
         public static final int about=0x7f06001f;
+        public static final int action_settings=0x7f06002a;
         public static final int app_name=0x7f06001a;
         /**   Title for notification shown when GooglePlayServices needs to be
         enabled for a application to work. [CHAR LIMIT=70] 
@@ -339,14 +353,17 @@ containing a value of this type.
         /**  Long form sign-in button text [CHAR LIMIT=30] 
          */
         public static final int common_signin_button_text_long=0x7f060013;
+        public static final int cotaInfo=0x7f06002c;
         public static final int cota_information=0x7f06001b;
         public static final int edit_message=0x7f060020;
+        public static final int hello_world=0x7f06002b;
         public static final int map=0x7f060027;
         public static final int nearby_stops=0x7f06001d;
         public static final int notifications=0x7f060026;
         public static final int permission_launch_activity=0x7f060028;
         public static final int preferences=0x7f06001e;
         public static final int title_activity_bus_line_schedule=0x7f060025;
+        public static final int title_activity_cota_information=0x7f060029;
         public static final int title_activity_main=0x7f060024;
     }
     public static final class style {
--- src/com/example/columbus/MainActivity.java
+++ src/com/example/columbus/MainActivity.java
@@ -38,18 +38,26 @@ public class MainActivity extends Activity {
 		return true;
 	}
 	@Override
+	
+	//Main Activity
 	public boolean onOptionsItemSelected(MenuItem item) {
 	    // Handle presses on the action bar items
 	    switch (item.getItemId()) {
 	        case R.id.cota_information:
 	            //link
-	            return true;
+	        	Intent intent = new Intent(MainActivity.this, CotaInformation.class);
+	        	this.startActivity(intent); 
+	            break;
 	        case R.id.cabs_information:
 	            //link
-	            return true;
+	        	Intent intentCabs = new Intent(MainActivity.this, CabsInformation.class);
+	        	this.startActivity(intentCabs); 
+	            break;
 	        case R.id.nearby_stops:
 	        	//
-	        	return true;
+	        	Intent intentStops = new Intent(MainActivity.this, NearestStops.class);
+	        	this.startActivity(intentStops); 
+	        	break;
 	        case R.id.preferences:
 	        	//
 	        	return true;
@@ -59,6 +67,7 @@ public class MainActivity extends Activity {
 	        default:
 	            return super.onOptionsItemSelected(item);
 	    }
+	    return true;
 	}
 	
 	//Called when the user clicks the Send button */
